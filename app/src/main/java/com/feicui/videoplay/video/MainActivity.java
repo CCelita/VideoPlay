624d0e48379c6f27c45ade05a38c36e9b8ff934e
xuliangbo <xuli@cui.com>
2016-10-19 14:58:49 +0800

    第二次提交

--- app/src/main/java/com/feicui/videoplay/video/MainActivity.java
+++ app/src/main/java/com/feicui/videoplay/video/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MyClass my=new MyClass();
         my.i="bb";
+        my.i="cc";
     }
 
 }
